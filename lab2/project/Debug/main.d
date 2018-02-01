bfa03e6bc275f39ada759cce8a9b19fe8e03f6bc
mc-roberts <34466202+mc-roberts@users.noreply.github.com>
2018-01-31 20:53:04 -0800

    Example project for ARM LED

---
+++ lab2/project/Debug/main.d
@@ -0,0 +1,17 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h
+main.obj: D:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+main.obj: D:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+main.obj: D:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+main.obj: D:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+
+../main.c: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h: 
+D:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+D:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+D:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+D:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
